474bb059fb280e0b6535f034bd50dfff06c31028
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2021-07-06 00:07:12 -0300

    update caderno Javascript
    Aprendendo condições simples em JS

---
+++ Guanabara - Javascript/exercicios/ex008.js
@@ -0,0 +1,8 @@
+//APERTE F8 PARA EXECUTAR NO CONSOLE
+console.log('o código funcionou perfeitamente')
+var vel = 80;
+//console.log(`A velocidade do seu carro é de ${vel} Km.`)
+if (vel > 60) { //condição simples
+    console.log(`A velocidade do seu carro é de ${vel} Km/h. Você foi MULTADO!`)
+}
+console.log('Dirija sempre usando cinto de segurança!')
