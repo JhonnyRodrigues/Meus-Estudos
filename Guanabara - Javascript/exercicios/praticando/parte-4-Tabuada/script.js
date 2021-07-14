2cf519c65ed9f06b466f583df3369fb67995e263
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2021-07-13 21:35:34 -0300

    exercicio 6 do curso de javascript Guanabara

---
+++ Guanabara - Javascript/exercicios/praticando/parte-4-Tabuada/script.js
@@ -0,0 +1,18 @@
+function geraTabuada() {
+
+let número = window.document.getElementById('txtn')
+let tabuada = window.document.getElementById('seltab')
+
+if (número.value.length == 0) { //verifica se foi digitado algum valor
+    window.alert('[ERRO] Por favor, insira um número!')    
+    } else {
+    let num = Number(número.value) //convertendo string para number
+    tabuada.innerHTML = ''  //limpa a lista
+    for(c = 1; c < 11; c++) {
+        let item = document.createElement('option') //cria uma tag <option>
+        item.text = `${num} x ${c} = ${num*c}`  //é possivel colocar expressões dentro de ${placeholder}
+        item.value = `tab${c}`  //nomeando cada novo tag <option> caso precise trabalhar com PHP
+        tabuada.appendChild(item)   //a tag <select> recebe a tag <option>
+        } 
+    }
+}
