d533f60710e5b677346f9f5db9dd6aad69b7a233
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2022-04-05 07:07:45 -0300

    Montando site Google Glass
    fim da Aula 06

---
+++ Guanabara - Javascript/desafios/desafio01/script.js
@@ -0,0 +1,12 @@
+function btn1(){
+    window.alert("Você clicou no 1º botão")
+}
+function btn2(){
+    window.alert("Você clicou no 2º botão")
+}
+function btn3(){
+    window.alert("Você clicou no 3º botão")
+}
+function btn4(){
+    window.close()
+}
