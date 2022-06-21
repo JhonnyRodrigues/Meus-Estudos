cae1326e4698a3f3b7a0f5f3a85f2a4c4237d8f4
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2022-06-21 07:14:36 -0300

    Canal_Ti - criando sistema de login

---
+++ Canal Ti/conexao.php
@@ -0,0 +1,8 @@
+<?php
+define('HOST','127.0.0.1'); //essa constante vai armazenar o IP do Banco de Dados
+define('USUARIO', 'root');
+define('SENHA', 'hakunamatata');
+define('DB','MeuBanco'); //essa constante vai armazenar o nome da Base de Dados
+
+$conexao = mysqli_connect();
+?>
