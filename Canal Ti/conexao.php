eaa8f1ebf81a9ed100c3d0c2a9492a1d1133b6d4
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2022-07-06 21:44:18 -0300

    Aula: Criando sistema de login com PHP e MySQL

--- Canal Ti/conexao.php
+++ Canal Ti/conexao.php
@@ -1,8 +1,10 @@
 <?php
 define('HOST','127.0.0.1'); //essa constante vai armazenar o IP do Banco de Dados
-define('USUARIO', 'root');
+define('USUARIO', 'root');  //armazena o usuario do Banco de Dados
 define('SENHA', 'hakunamatata');
 define('DB','MeuBanco'); //essa constante vai armazenar o nome da Base de Dados
 
-$conexao = mysqli_connect();
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('ERRO: Não foi possível conectar'); //a variável armazena as constantes através da função e, caso haja erro, apresenta uma string de ERRO
+
+
 ?>
