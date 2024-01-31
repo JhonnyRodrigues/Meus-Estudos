1fb19d4b05f4f01c51423b081a65b9d7f6a6e5fc
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-01-31 09:37:53 -0300

    Update snippet_manipular_elementos.js
    aplicando promises

--- Snippets/snippet_manipular_elementos.js
+++ Snippets/snippet_manipular_elementos.js
@@ -134,3 +134,14 @@ window.document.querySelector('div#mensagem_sincronizacao').style.setProperty('f
 //capturar elemento <select> com atributo específico (o problema da diferente sintaxe entre o CSS:checked e o JS.selected)
 /* 1° opção: */ document.querySelector('#id_sc_field_situacao > option[value="4"]:checked')
 /* 2° opção: */ document.querySelector('#id_sc_field_situacao > option[value="4"]').selected
+
+
+
+//Usando PROMISES para executar função após término de outra função
+function funcao1() { return new Promise(resolve => { /* Lógica da função 1 */ }); }
+function funcao2() { /* Lógica da função 1 */ }
+funcao1().then(() => { funcao2(); });
+/* Exemplo usando função da biblioteca SweetAlert2: */
+Swal.fire({text: "Não é permitido assinar esta demanda antes do Diretor responsável!"}).then(
+	() => { window.parent.tb_remove(); }
+);
