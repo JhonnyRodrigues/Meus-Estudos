96872c9d0afd789f63ecebee315c50467d1242dc
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-02-05 17:02:31 -0300

    Create explicação de como utilizar a função array_map.php
    entendendo função PHP array_map()

---
+++ Snippets/explicação de como utilizar a função array_map.php
@@ -0,0 +1,33 @@
+<?php
+
+// Objetivo: transformar array(array("106"),array("623"))   em  array("106", "623") 
+
+var_dump({datasetSuperFinancas});
+/* Retorna:
+array(2) {
+  [0]=>
+  array(1) {
+    ["COD_FUNCIONARIO"]=>
+    string(3) "106"
+  }
+  [1]=>
+  array(1) {
+    ["COD_FUNCIONARIO"]=>
+    string(3) "623"
+  }
+}
+*/
+
+var_dump(array_map(function ($arg) {return $arg['COD_FUNCIONARIO'];}, {datasetSuperFinancas}));
+/* Retorna:
+array(2) {
+  [0]=>
+  string(3) "106"
+  [1]=>
+  string(3) "623"
+}
+*/
+
+// Explicação: "Para cada elemento contido no array {datasetSuperFinancas}, retorne o valor contido em sua chave 'COD_FUNCIONARIO'"
+
+?>
