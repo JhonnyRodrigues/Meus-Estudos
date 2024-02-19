372a4db62f85a763b49a83245e4ab4ac6fa92c54
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-02-19 09:18:12 -0300

    Update snippet_criar_arquivo_logs.php

--- Snippets/snippet_criar_arquivo_logs.php
+++ Snippets/snippet_criar_arquivo_logs.php
@@ -1,16 +1,27 @@
 <?php
 
 #ARQUIVO LOGS
-// unlink("../logs/lembretes_solicitacoes.txt");
+// unlink("../logs/lembretes_solicitacoes.txt");exit;
 if (!is_dir('../logs')) {
-    mkdir('../logs', 0755);
-}
-$file = fopen("../logs/lembretes_solicitacoes.txt", "a+");
-if (!$file) {
-    exit("Falha ao abrir o arquivo");
-}
-if (filesize("../logs/lembretes_solicitacoes.txt") == 0) {
-    fwrite($file, "DATA e HORA --- USUÁRIO LOGADO --- E-MAIL DO DESTINATÁRIO");
+    if (!mkdir('../logs', 0755)) {
+        errorMessage("Falha ao criar o diretório que armazena os logs de envio de e-mails lembretes aos solicitantes.", 5);
+    } else {
+        // echo 'Diretório criado com sucesso!';
+        $file = fopen("../logs/lembretes_solicitacoes.txt", "a+"); //modo de criacao
+        if (!$file) {
+            errorMessage("Falha ao abrir o arquivo que armazena os logs de envio de e-mails lembretes aos solicitantes.", 6);
+        } else if (filesize("../logs/lembretes_solicitacoes.txt") == 0) {
+            fwrite($file, "DATA e HORA --- USUÁRIO LOGADO --- E-MAIL DO DESTINATÁRIO"); //cabecalho
+        }
+    }
+} else {
+    // echo 'O diretório já existe.';
+    $file = fopen("../logs/lembretes_solicitacoes.txt", "a+"); //modo de criacao
+    if (!$file) {
+        errorMessage("Falha ao abrir o arquivo que armazena os logs de envio de e-mails lembretes aos solicitantes.", 6);
+    } else if (filesize("../logs/lembretes_solicitacoes.txt") == 0) {
+        fwrite($file, "DATA e HORA --- USUÁRIO LOGADO --- E-MAIL DO DESTINATÁRIO"); //cabecalho
+    }
 }
 fwrite($file, "\n" . date('d/m/Y H:i:s') . ' - ' . $_SESSION['usr_login'] . ' - ' . {datasetSolicitAguardando}->fields['EMAIL']);
 fclose($file);
