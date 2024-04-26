43a66deeb161ed3c2da72f413b243483b05018b4
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-04-26 11:31:55 -0300

    update snippet exemplo encontrar registros duplicados

--- Snippets/exemplo encontrar registros duplicados.sql
+++ Snippets/exemplo encontrar registros duplicados.sql
@@ -1,13 +1,53 @@
+-- lista os registros duplicados, porém com alto custo de processamento (nao recomendado)
 SELECT
 	*
 FROM
-	PAC_ASSINATURAS A
+	TABELA A
 WHERE
 	(
 		SELECT
-			COUNT(A2.ID_ASSINATURA)
+			COUNT(*)
 		FROM
-			PAC_ASSINATURAS A2
+			TABELA A2
 		WHERE
-			A2.FK_DEMANDA = A.FK_DEMANDA
+			A2.FK_CAMPO = A.FK_CAMPO
 	) > 1
+ORDER BY
+	A.CAMPO1
+
+--  lista os registros duplicados, com baixo custo de processamento (recomendado)
+SELECT
+	*
+FROM
+	HELP_HISTORICOS
+WHERE
+	FK_SOLICITACAO IN (
+		SELECT
+			FK_SOLICITACAO
+		FROM
+			HELP_HISTORICOS
+		WHERE
+			FK_STATUS = 4
+		GROUP BY
+			FK_SOLICITACAO
+		HAVING
+			COUNT(*) > 1
+	)
+	AND FK_STATUS = 4
+ORDER BY
+	FK_SOLICITACAO,
+	ID_HISTORICO
+
+
+-- agrupa os registros duplicados
+SELECT
+	FK_SOLICITACAO,
+	COUNT(*)
+FROM
+	HELP_HISTORICOS
+GROUP BY
+	FK_SOLICITACAO
+HAVING
+	COUNT(*) > 1
+
+-- 
