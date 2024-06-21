ed3cff090a6eea0cba14898019bc51d2fd326835
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-06-21 14:29:10 -0300

    Create snippet_openModal.js

---
+++ Snippets/snippet_openModal.js
@@ -0,0 +1,58 @@
+//1 abre modal SweetAlert para inserir motivo
+//2 joga esse motivo em variável PHP
+//3 altera o estado do botão
+//4 clica no botão para invocar processamento PHP
+
+async function openModalMotivo(btnName, row, fieldParamName, newState,placeholder) {
+	const motivo = await Swal.fire(
+		{
+			title: 'Confirma a prescrição da relação entre o contrato e este equipamento?',
+			input: "textarea",
+			inputPlaceholder: placeholder,
+			confirmButtonText: 'Enviar',
+			showCancelButton: true,
+			cancelButtonText: 'Cancelar',
+			inputValidator: (value) => {
+				return new Promise((resolve) => { //A promise aguarda a invocação do método 'resolve'
+					if ((value.trim() !== '') && (value !== null) && (value !== undefined) && (value.length <= 1000)) {
+						resolve();
+					} else if (value.lenght > 1000) {
+						resolve("O tamanho máximo do motivo é de 1000 caracteres.");
+					} else {
+						resolve("É obrigatório informar o motivo para prescrever a relação entre equipamento e contrato!");
+					}
+				});
+			}
+		}
+	);
+	
+	if (motivo.value == '' || motivo.value == null) {
+		return;
+	}
+	
+	const motivoEncoded = encodeURIComponent(motivo.value);
+	
+	const fieldParam = document.querySelector('#id_sc_field_' + fieldParamName + '_' + row); //campo virtual scriptcase
+	const btnSend = document.querySelector('#sc-actionbar-actbtn_' + btnName + '_' + row); //botão 'Enviar'
+	
+	if (fieldParam && btnSend) {
+		fieldParam.innerHTML = motivoEncoded; //atribui o motivo codificado ao campo virtual scriptcase
+		btnSend.dataset.actionbarState = newState; //altera o valor do atributo do objeto HTML element
+		$('#sc-actionbar-actbtn_' + btnName + '_' + row).data('actionbarState', newState); //altera o valor do atributo do objeto JQuery... contido no objeto HTML element acima
+		btnSend.click();
+	}
+}
+	
+document.addEventListener('DOMContentLoaded', function() {
+	document.querySelectorAll('[id^=sc-actionbar-actbtn_prescrever]').forEach(
+		(btn) => {
+			const numRow = btn.getAttribute('data-actionbar-row').replace('_','');
+
+			btn.addEventListener('click', function() {
+				if (btn.dataset.actionbarState == 'pendente') {
+					openModalMotivo('prescrever', numRow, 'param_motivo_prescricao', 'prescrevendo', 'Insira o motivo da prescrição');
+				}
+			});
+		}
+	);
+});
