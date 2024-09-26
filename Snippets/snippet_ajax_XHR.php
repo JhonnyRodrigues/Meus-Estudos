095e3852a57e2b6ea852d97d644698c3e3527b93
Jhonny Rodrigues <jonatas.rdp@gmail.com>
2024-09-26 10:21:55 -0300

    Update snippet_ajax_XHR.php

--- Snippets/snippet_ajax_XHR.php
+++ Snippets/snippet_ajax_XHR.php
@@ -1,12 +1,31 @@
 <?php
 if (isset($_POST['name']) && $_POST['name'] != '') {
-    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-    $arr = [
-        'name' => $name,
-        'age' => 32,
-        'city' => 'Piracicaba'
-    ];
-    echo json_encode($arr);
+    #primeiramente simulando um json (db.json)
+    if ($_POST['name'] == 'carro') {
+        $veiculo = '{
+            "car":
+              [
+                {
+                "color":"black",
+                "year":2018
+                },
+                {
+                  "color":"red",
+                  "year":2007
+                }
+              ]
+            }';
+        echo json_encode($veiculo);
+    #simulando retorno em array de uma query
+    } else {
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $pessoa = [
+            'name' => $name,
+            'age' => 32,
+            'city' => 'Piracicaba'
+        ];
+        echo json_encode($pessoa);
+    }
     exit;
 }
 ?>
@@ -22,9 +41,11 @@ if (isset($_POST['name']) && $_POST['name'] != '') {
 
 <body>
     <form name="form" id="form" method="post" action="$_SERVER['PHP_SELF']">
-        <input type="text" name="name" id="name" placeholder="Write your name"><br>
-        <input type="submit" value="Enviar">
-        <div id="resposta"></div>
+        <label for="name">Palavra: </label>
+        <input type="text" name="name" id="name" placeholder="digite a palavra 'carro' (ou outra qualquer)" size="35">
+        <input type="submit" value="Enviar"><br>
+        <label for="resposta"><strong>Retorno: </strong></label>
+        <span id="resposta"></span>
     </form>
 
     <script>
@@ -32,24 +53,29 @@ if (isset($_POST['name']) && $_POST['name'] != '') {
             'use strict'; //Habilita o modo estrito dentro da função para garantir que o código seja mais seguro e menos propenso a erros.
 
             let form = doc.querySelector('#form');
-            let divResposta = doc.querySelector('div#resposta');
+            let spanResposta = doc.querySelector('span#resposta');
 
             //Send ajax form
             function sendForm(event) {
                 event.preventDefault();
-                let ajax = new XMLHttpRequest();
+                let ajax = new XMLHttpRequest(); //#1
                 let params = 'name=' + doc.querySelector('#name').value;
-                ajax.open('POST', '<?= $_SERVER["PHP_SELF"]; ?>', true); //$_SERVER["PHP_SELF"] retorna o caminho do script atual em execução
+                ajax.open('POST', '<?= $_SERVER["PHP_SELF"]; ?>', true); //#2 ($_SERVER["PHP_SELF"] retorna o caminho do script atual em execução)
                 ajax.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                ajax.onreadystatechange = function() {
+                ajax.onreadystatechange = function() { //#3
                     if (ajax.status === 200 && ajax.readyState === 4) {
                         let json = JSON.parse(ajax.responseText);
-                        let retorno = 'nome: ' + json.name + ' - idade: ' + json.age + ' - cidade: ' + json.city;
-                        console.log(retorno);
-                        divResposta.innerHTML = retorno;
+                        if (doc.querySelector('input#name').value == 'carro') {
+                            console.log(json);
+                            spanResposta.innerHTML = json;
+                        } else {
+                            let retorno = 'nome: ' + json.name + ' - idade: ' + json.age + ' - cidade: ' + json.city;
+                            console.log(retorno);
+                            spanResposta.innerHTML = retorno;
+                        }
                     }
                 };
-                ajax.send(params);
+                ajax.send(params); //#4
             }
             form.addEventListener('submit', sendForm, false);
 
