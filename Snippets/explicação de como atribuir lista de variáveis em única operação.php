a88a84c393d0901582838408d57682163317a336
Jhonny <jonatas.rdp@gmail.com>
2025-03-20 10:09:09 -0300

    update snippet atribuir lista de variáveis

--- Snippets/explicação de como atribuir lista de variáveis em única operação.php
+++ Snippets/explicação de como atribuir lista de variáveis em única operação.php
@@ -13,9 +13,14 @@ $contas = [
         'saldo' => 800
     ],
 ];
-
 #MÉTODO PARA DEFINIR AS VARIÁVEIS $titular E $saldo
 foreach ($contas as ['titular' => $titular, 'saldo' => $saldo]) {
     echo "$titular possui $saldo reais" . PHP_EOL;
 }
+
+
+
+#OUTRO EXEMPLO COM SINTAXE ALTERNATIVA
+$result = $pdo->query("SELECT id, name FROM employees");
+list($id, $name) = $result→fetch(PDO::FETCH_NUM);
 /*Nesse caso nós estamos usando a sintaxe resumida usando colchetes, mas poderíamos sem problema usar a sintaxe mais verbosa com com list().*/
