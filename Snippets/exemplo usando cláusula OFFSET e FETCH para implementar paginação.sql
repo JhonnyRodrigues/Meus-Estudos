97afeb0beedff1febc72ffca34158ea4ecabe105
Jhonny <jonatas.rdp@gmail.com>
2025-10-30 20:42:52 -0300

    update caderno SQL
    + rename snippets
    + learning PostgreSQL

--- Snippets/exemplo usando cláusula OFFSET e FETCH para implementar paginação.sql
+++ Snippets/exemplo usando cláusula OFFSET e FETCH para implementar paginação.sql
@@ -5,5 +5,12 @@ FROM
     RH_FUNCIONARIOS
 ORDER BY
     COD_FUNCIONARIO 
-OFFSET 5 ROWS 
-FETCH NEXT 5 ROWS ONLY
+OFFSET 5 ROWS -- Pula as primeiras 5 linhas do resultado.
+FETCH NEXT 5 ROWS ONLY --traz as 5 próximas linhas após o offset
+
+
+-- Pegar a primeira página (10 registros)
+SELECT *
+FROM clientes
+ORDER BY id_cliente
+FETCH FIRST 10 ROWS ONLY;
